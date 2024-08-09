07a3436bf0ea93dc3ba5be18d8481d4f03986cbb
JorMath <94020880+JorMath@users.noreply.github.com>
2024-08-09 10:55:46 -0500

    Movimiento y transferencia POJO

--- src/main/java/modelo/entidades/Movimiento.java
+++ src/main/java/modelo/entidades/Movimiento.java
@@ -1,22 +1,73 @@
-package modelo .entidades;
+package modelo.entidades;
 
+import java.io.Serializable;
 import java.util.*;
 
-/**
- * 
- */
-public class Movimiento {
-	private int id;
-    private String concept;
-    private Date date;
-    private double value;
-    /**
-     * Default constructor
-     */
-    public Movimiento() {
-    }
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_movimiento", discriminatorType = DiscriminatorType.STRING)
+public class Movimiento implements Serializable {
 
-    
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column
+	private String concept;
+	@Column
+	private Date date;
+	@Column
+	private double value;
+
+	public Movimiento() {
+	}
+
+	public Movimiento(String concept, Date date, double value) {
+		this.concept = concept;
+		this.date = date;
+		this.value = value;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getConcept() {
+		return concept;
+	}
+
+	public void setConcept(String concept) {
+		this.concept = concept;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
 
 }
--- src/main/java/modelo/entidades/Transferencia.java
+++ src/main/java/modelo/entidades/Transferencia.java
@@ -2,6 +2,7 @@ package modelo.entidades;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Date;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -14,55 +15,25 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 
 @Entity
-public class Transferencia implements Serializable {
+public class Transferencia extends Movimiento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	@Column
-	private double amount;
 	@ManyToOne(cascade = CascadeType.PERSIST)
 	private Cuenta srcAccount;
 	@ManyToOne(cascade = CascadeType.PERSIST)
 	private Cuenta dstAccount;
-	@Column
-	private LocalDate date;
-	@Column
-	private String concept;
-	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "idCategoriaTransferencia")
 	private CategoriaTransferencia categoriaTransferencia;
 
 	public Transferencia() {
 
 	}
-
-	public Transferencia(double amount, Cuenta srcAccount, Cuenta dstAccount, LocalDate date, String concept,
-			CategoriaTransferencia categoriaTransferencia) {
-		this.amount = amount;
+	
+	public Transferencia(String concept, Date date, double value, Cuenta srcAccount, Cuenta dstAccount, CategoriaTransferencia categoriaTransferencia) {
+		super(concept, date, value);
 		this.srcAccount = srcAccount;
 		this.dstAccount = dstAccount;
-		this.date = date;
-		this.concept = concept;
 		this.categoriaTransferencia = categoriaTransferencia;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
 	}
 
 	public Cuenta getSrcAccount() {
@@ -81,22 +52,7 @@ public class Transferencia implements Serializable {
 		this.dstAccount = dstAccount;
 	}
 
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-
-	public String getConcept() {
-		return concept;
-	}
-
-	public void setConcept(String concept) {
-		this.concept = concept;
-	}
-
+	
 	public CategoriaTransferencia getCategoriaTransferencia() {
 		return categoriaTransferencia;
 	}
