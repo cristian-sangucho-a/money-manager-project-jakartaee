7c749037ea545604f2c635ee1f43044c6ae63716
Cristian-Zambrano <cristian.zambrano@epn.edu.ec>
2024-08-08 23:25:49 -0500

    create: clase dao para el Egreso

---
+++ src/main/java/modelo/dao/EgresoDAO.java
@@ -0,0 +1,14 @@
+package modelo.dao;
+
+import java.util.Date;
+
+import modelo.entidades.CategoriaEgreso;
+
+public class EgresoDAO {
+
+	public void registerExpense(Date date, String concept, double value, CategoriaEgreso category) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
