d3c51bda82c77192e44bc4c74dc152d7c5b65d11
Cristtsirc <cristian.sangucho@epn.edu.ec>
2024-08-09 18:42:22 -0500

    feat: registrar ingreso

---
+++ src/main/java/modelo/dao/IngresoDAO.java
@@ -0,0 +1,59 @@
+package modelo.dao;
+
+import java.io.*;
+import java.util.*;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import modelo.entidades.CategoriaEgreso;
+import modelo.entidades.CategoriaIngreso;
+import modelo.entidades.Cuenta;
+import modelo.entidades.Egreso;
+import modelo.entidades.Ingreso;
+
+/**
+ * 
+ */
+public class IngresoDAO extends MovimientoDAO {
+	private EntityManagerFactory emf = null;
+	private EntityManager em = null;
+	
+	public IngresoDAO() {
+		this.emf = Persistence.createEntityManagerFactory("Contabilidad");
+    	this.em = emf.createEntityManager();
+	}
+	
+    public void getAllByAccount() {
+        // TODO implement here
+    }
+
+    /**
+     * @param from 
+     * @param to
+     */
+    public void getAll(void from, void to) {
+        // TODO implement here
+    }
+
+    /**
+     * @param date 
+     * @param concept 
+     * @param value 
+     * @param category 
+     * @param dstID
+     */
+
+    public void registerIncome(Date date, String concept, double value, CategoriaIngreso incomeCategory, Cuenta dstID) {
+    	em.getTransaction().begin();
+		try {
+			em.persist(new Ingreso(concept,date,value,dstID,incomeCategory));
+			em.getTransaction().commit();
+		}catch(Exception e) {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+		}
+    }
+
+}
