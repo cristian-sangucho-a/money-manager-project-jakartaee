e9a1476bb5cf7ca3a77936e54db19489305511ab
Cristian-Zambrano <cristian.zambrano@epn.edu.ec>
2024-08-08 21:25:13 -0500

    Fix: Cuenta

--- src/main/java/modelo/entidades/Cuenta.java
+++ src/main/java/modelo/entidades/Cuenta.java
@@ -1,38 +1,43 @@
-package modelo .entidades;
+package modelo.entidades;
 
 import java.io.Serializable;
-import java.util.*;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table
 public class Cuenta implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	private Integer id;
+	
+	@Column
+	private String name;
+	@Column
+	private double balance;
+	
+	public Cuenta() {
+		
+	}
 
-    @Column
-    private String name;
-
-    @Column
-    private double balance;
-    
-    public Cuenta() {
-    }
-
-	public Cuenta(int id, String name, double balance) {
+	public Cuenta(Integer id, String name, double balance) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.balance = balance;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -51,5 +56,5 @@ public class Cuenta implements Serializable{
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
-    
+
 }
