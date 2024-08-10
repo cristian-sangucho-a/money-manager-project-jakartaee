514632438829dfb2626a7f04f020af53d4c513f6
Cristian-Zambrano <cristian.zambrano@epn.edu.ec>
2024-08-09 19:02:35 -0500

    fix: letra mal mandada

--- src/main/java/modelo/dao/CategoriaEgresoDAO.java
+++ src/main/java/modelo/dao/CategoriaEgresoDAO.java
@@ -38,7 +38,7 @@ public class CategoriaEgresoDAO extends CategoriaDAO {
             query.setParameter(1, from);
             query.setParameter(2, to);
             List<Object[]> results = query.getResultList();
-            for (Object[] result : results) {n
+            for (Object[] result : results) {
                 String name = (String) result[0];
                 Double sum = (Double) result[1];
                 resultList.add(new CategoriaResumenDTO(name, sum));
