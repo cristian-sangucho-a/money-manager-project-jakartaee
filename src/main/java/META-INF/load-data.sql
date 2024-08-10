f3426c1fc5a9e189550b3983d02d0672b405ff75
Cristtsirc <cristian.sangucho@epn.edu.ec>
2024-08-10 17:08:00 -0500

    chore: add test data

---
+++ src/main/java/META-INF/load-data.sql
@@ -0,0 +1,17 @@
+INSERT INTO CategoriaIngreso (ID, NAME) VALUES (1, 'Salario');
+INSERT INTO CategoriaIngreso (ID, NAME) VALUES (2, 'Intereses');
+INSERT INTO CategoriaIngreso (ID, NAME) VALUES (3, 'Servicios');
+
+INSERT INTO CategoriaEgreso (ID, NAME) VALUES (1, 'Alimentacion');
+INSERT INTO CategoriaEgreso (ID, NAME) VALUES (2, 'Transporte');
+INSERT INTO CategoriaEgreso (ID, NAME) VALUES (3, 'Golosina');
+INSERT INTO CategoriaEgreso (ID, NAME) VALUES (4, 'Guaguas');
+
+INSERT INTO CategoriaTransferencia (ID, NAME) VALUES (1, 'Transferencia');
+
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (1, 'Bco. Pichincha ahorros', 1000.00);
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (2, 'Bco. Pacifico ahorro', 5000.00);
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (3, 'Efetivo', 20.00);
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (4, 'Bco. Pichincha corriente', 1000.00);
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (5, 'Bco. Pacifico corriente', 5000.00);
+
