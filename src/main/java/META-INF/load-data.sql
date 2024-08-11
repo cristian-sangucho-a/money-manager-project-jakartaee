410b34ae619363f0c69c76d7945b7319cc9b3363
Cristtsirc <cristian.sangucho@epn.edu.ec>
2024-08-10 22:35:42 -0500

    fix: test data

--- src/main/java/META-INF/load-data.sql
+++ src/main/java/META-INF/load-data.sql
@@ -10,8 +10,8 @@ INSERT INTO CategoriaEgreso (ID, NAME) VALUES (4, 'Guaguas');
 INSERT INTO CategoriaTransferencia (ID, NAME) VALUES (1, 'Transferencia');
 
 INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (1, 'Bco. Pichincha ahorros', 1000.00);
-INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (2, 'Bco. Pacifico ahorro', 5000.00);
-INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (3, 'Efetivo', 20.00);
-INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (4, 'Bco. Pichincha corriente', 1000.00);
-INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (5, 'Bco. Pacifico corriente', 5000.00);
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (2, 'Bco. Pacifico corriente', 2000.00);
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (3, 'Efectivo', 20.00);
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (4, 'Bco. Pichincha corriente', 3000.00);
+INSERT INTO Cuenta (ID, NAME, BALANCE) VALUES (5, 'Bco. Pacifico corriente', 4000.00);
 
