d16bfd9f0b27b050a2d5f009e532b47993c573d8
Jean Cotera <jean.cotera@gmail.com>
2024-08-12 21:38:13 -0500

    Limpieza de codigo

--- src/main/java/modelo/dao/CategoriaDAO.java
+++ src/main/java/modelo/dao/CategoriaDAO.java
@@ -69,33 +69,4 @@ public class CategoriaDAO {
 		return null;
 	}
 	
-	public List<CategoriaResumenDTO> getAllSumarized(Date from, Date to) {
-    	EntityManager em = ManejoEntidadPersistencia.getEntityManager();
-        List<CategoriaResumenDTO> resultList = new ArrayList<>();
-        try {
-            String queryStr = "SELECT c.name, SUM(m.valor), c.id "
-            		+ "FROM categoria c "
-            		+ "LEFT JOIN movimiento m ON c.id = m.Categoria_ID "
-            		+ "AND m.fecha BETWEEN ?1 AND ?2 "
-            		+ "GROUP BY c.id;";
-            Query query = em.createNativeQuery(queryStr);
-            query.setParameter(1, from);
-            query.setParameter(2, to);
-            List<Object[]> results = query.getResultList();
-            for (Object[] result : results) {
-                String name = (String) result[0];
-                Double sum = (result[1] == null) ? 0 : (Double) result[1];
-                int id = (Integer) result[2];
-                resultList.add(new CategoriaResumenDTO(name, sum, id));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (em != null) {
-                em.close();
-            }
-        }
-        return resultList;
-    }
-
 }
